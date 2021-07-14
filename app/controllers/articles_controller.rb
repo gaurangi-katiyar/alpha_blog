677c15280da93b65afbf5e86ea0ec39759b5213c
Gaurangi Katiyar <gaurangi.katiyar@anchanto.com>
2021-07-14 17:01:00 +0530

    crud operations except destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,33 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
+  def index
+    @articles = Article.all
+  end
+  def new
+    @articles = Article.new
+  end
+  def edit
+    @articles = Article.find(params[:id])
+  end
+  def create
+    #render plain: params[:article]
+    @articles = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @articles.inspect
+    if @articles.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @articles
+    else
+      render 'new'
+    end
+  end 
+  def update
+    @articles = Article.find(params[:id])
+    if @articles.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated"
+      redirect_to @articles
+    else
+      render 'edit'
+    end
+  end
 end
