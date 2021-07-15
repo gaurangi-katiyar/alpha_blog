93655dd47ec2ec2d7f33945c7fcef3a067bec1df
Gaurangi Katiyar <gaurangi.katiyar@anchanto.com>
2021-07-15 11:13:36 +0530

    ALL CRUD OPERATIONS

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
+    #@articles = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -10,11 +12,11 @@ class ArticlesController < ApplicationController
     @articles = Article.new
   end
   def edit
-    @articles = Article.find(params[:id])
+    #@articles = Article.find(params[:id])
   end
   def create
     #render plain: params[:article]
-    @articles = Article.new(params.require(:article).permit(:title, :description))
+    @articles = Article.new(article_params)
     #render plain: @articles.inspect
     if @articles.save
       flash[:notice] = "Article was created successfully"
@@ -24,12 +26,26 @@ class ArticlesController < ApplicationController
     end
   end 
   def update
-    @articles = Article.find(params[:id])
-    if @articles.update(params.require(:article).permit(:title, :description))
+    #@articles = Article.find(params[:id])
+    if @articles.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to @articles
     else
       render 'edit'
     end
   end
+  def destroy
+    #@articles = Article.find(params[:id])
+    @articles.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @articles = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
