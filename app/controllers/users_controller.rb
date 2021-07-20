899e60436ee2ffbb5178e3de637eb6e9a84ce648
Gaurangi Katiyar <gaurangi.katiyar@anchanto.com>
2021-07-20 13:07:40 +0530

    Add users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-private
 
-def user_params
-  params.require(:user).permit(:username, :email, :password)
-end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Successfully updated"
+    else
+      render 'edit'
+    end 
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
